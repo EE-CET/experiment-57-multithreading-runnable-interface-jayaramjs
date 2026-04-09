cb84d9ba55aafa0441e29b99aa27a01816afa141
Jayaram J S <89035356+jayaramjs@users.noreply.github.com>
2026-04-09 20:36:36 +0530

    Implement Runnable interface and start thread

--- src/RunnableThread.java
+++ src/RunnableThread.java
@@ -1,12 +1,25 @@
+// Step 1: Implement the Runnable interface
 class MyRunnable implements Runnable {
-    // TODO: Override run() method
-    // Loop from 1 to 5 and print numbers separated by space
+    // Step 2: Override the run() method
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            // Printing numbers 1 to 5 separated by space
+            System.out.print(i + " ");
+        }
+    }
 }
 
 public class RunnableThread {
     public static void main(String[] args) {
-        // TODO: Create an instance of MyRunnable
-        // TODO: Create a Thread object passing the runnable instance
-        // TODO: Start the thread
+        // Step 3: Create an instance of the class that implements Runnable
+        MyRunnable myTask = new MyRunnable();
+
+        // Step 4: Create a Thread object and pass the Runnable instance to its constructor
+        // The Thread object is the "worker," and the Runnable is the "job."
+        Thread t1 = new Thread(myTask);
+
+        // Step 5: Start the thread
+        t1.start();
     }
 }
